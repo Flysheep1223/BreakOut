4b491a9409eadfd71aa830c29013e7abecdc2fd5
Flysheep1223 <q3315513581@163.com>
2025-12-04 15:53:25 +0800

    将 Static Boss (Hidden Bee) 集成到 AI 管理器中，添加初始化 (init_static_boss) 和每回合更新 (static_boss_tick) 的调用。

--- enemies/ai_manager.pl
+++ enemies/ai_manager.pl
@@ -1,11 +1,13 @@
-:- dynamic chaser/3, random_walker/5.
+:- dynamic chaser/3, random_walker/5, static_boss/5.
 
 % Initialize enemies
 init_enemies :-
     init_chaser(10, 2),
-    init_random_walker(40, 10).
+    init_random_walker(40, 10),
+    init_static_boss.
 
 % Global tick for all enemies
 enemies_tick :-
     chaser_tick,
-    random_walker_tick.
+    random_walker_tick,
+    static_boss_tick.
